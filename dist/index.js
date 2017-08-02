18a1e5e86abf09b211adf351783cec61e8c0f538
njgingrich <njgingrich@gmail.com>
2017-08-01 21:30:33 -0400

    removed build dir from git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function installEndpoints(app) {
-    return false;
-}
-exports.installEndpoints = installEndpoints;
-//# sourceMappingURL=index.js.map
