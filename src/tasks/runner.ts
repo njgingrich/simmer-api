d268ef35960417e66aff5731b24b71b8b3d99b96
nathan <njgingrich@gmail.com>
2017-08-24 20:43:13 -0400

    run tasks automatically based on date

--- src/tasks/runner.ts
+++ src/tasks/runner.ts
@@ -3,20 +3,24 @@ import { QueryResult } from 'pg'
 import * as db from '../db'
 import { Task, TASK_STATUS } from './Task'
 
+export interface TaskItem {
+  task: Task
+  time: number
+}
+
 export class TaskRunner {
   tasks_count: number
-  tasks: Task[]
+  tasks: TaskItem[]
 
   constructor() {
     this.tasks_count = 0
     this.tasks = []
+    this.runTasks()
   }
 
-  runTask(task: Task): void {
-    this.updateTask(task, TASK_STATUS.ACTIVE)
-      .then(() => {
-        return task.performTask()
-      })
+  runTask(task: Task): Promise<any> {
+    return task
+      .performTask()
       .then(success => {
         winston.log('info', `Performed Task ${task.id}, which ${success ? 'succeeded' : 'failed'}`)
         return task.useResults()
@@ -25,22 +29,37 @@ export class TaskRunner {
         return winston.log('info', `Used Results of Task ${task.id}, which ${success ? 'succeeded' : 'failed'}`)
       })
       .then(() => {
-        this.updateTask(task, TASK_STATUS.COMPLETE)
+        return this.updateTask(task, TASK_STATUS.COMPLETE)
       })
+  }
+
+  runTasks(): void {
+    setInterval(() => {
+      console.log('Looking for tasks...')
+      this.tasks.forEach((item, i) => {
+        if (item.time <= new Date().getTime()) {
+          console.log(`Running task ${item.task.id}`)
+          this.updateTask(item.task, TASK_STATUS.ACTIVE)
+            .then(() => {
+              return this.runTask(item.task)
+            })
+            .then(() => {
+              console.log(`Removing task ${item.task.id} w/ index ${i}`)
+              this.tasks.splice(i, 1)
+            })
+        }
+      })
+    }, 4000)
   }
 
   scheduleTask(task: Task, time: Date): void {
     this.tasks_count += 1
     task.id = this.tasks_count
-    db
-      .putTask(task)
-      .then(() => {
-        this.tasks.push(task)
-        this.updateTask(task, TASK_STATUS.SCHEDULED)
-      })
-      .then(() => {
-        this.runTask(task)
-      })
+    db.putTask(task).then(() => {
+      console.log(`adding task ${task.id}`)
+      this.tasks.push({ task, time: time.getTime() })
+      this.updateTask(task, TASK_STATUS.SCHEDULED)
+    })
   }
 
   updateTask(task: Task, status: number): Promise<QueryResult> {
